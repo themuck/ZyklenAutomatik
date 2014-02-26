49f140e4440d52c98554266f43580e07e6a389e5
Soenke J. Peters <peters__git@opcenter.de>
2014-02-26 21:46:57 +0100

    Added Makefile, header file

---
+++ src/Bresenham_Shield_muck/Bresenham_Shield_muck.h
@@ -0,0 +1,18 @@
+void print_menue ();
+void print_edit_cursor ();
+void print_menue_numbers ();
+void doEncoderB();
+void doSpindleA();
+void doSpindleB();
+void doSpindleZ();
+void sm_driver_StepCounter();
+void sm_driver_StepOutput();
+void bresenham_StepOutput();
+void speed_cntr_Move(signed int step, unsigned int speed);
+void speed_cntr_Init_Timer1(void);
+void io_init();
+long read_edit_number();
+void trigger_edit_number(int value);
+void write_edit_number(int value);
+void write_default_config();
+void decl_trigger();
